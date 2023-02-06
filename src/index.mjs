2f327e8fda40ce6bca5f64d9805800209d8194f1
cayolegal <cayo.legal@innovatica.com.py>
2023-02-05 21:37:09 -0300

    changes

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,4 @@
 import {
-  fluorescentTubeUnits,
   hoursPerDay,
   daysPerWeek,
   weeksPerMonth,
@@ -9,35 +8,45 @@ import {
 import {setupInitialValues, buyNewTubesBox} from "./utils.mjs";
 
 let totalTimeInHours = monthsPerYear * weeksPerMonth * daysPerWeek * hoursPerDay;
-//let totalTimeInHours = 30;
 
-let {total, tubes} = setupInitialValues({
-  fluorescentTubeUnits,
-});
-
-console.log('tubos =>', tubes)
+let {total, tubesBox} = setupInitialValues()
 
 let totalTubesBroken = 0;
-let boxIdCounter = tubes.length + 1;
+let boxIdCounter = tubesBox.length + 1;
 
 while(totalTimeInHours > 0) {
-  tubes.forEach((t, i) => {
-    let tubesBrokenInTheBox = 0;
+
+  // looping each box unit
+  tubesBox.forEach((t, i) => {
+    let tubesBrokenInCurrentBox = 0;
+
+    // looping each tube of the box
     Object.keys(t).forEach((key) => {
-      if(tubes[i][key] > 0) {
-        tubes[i] = {
-          ...tubes[i],
-          [key]: key !== "id" ? tubes[i][key] - 1 : tubes[i][key],
+
+      // we ask if the tube still works, if not, we no continue to use it
+      if (tubesBox[i][key] > 0) {
+        tubesBox[i] = {
+          ...tubesBox[i],
+
+          // here we substract each hour of the tube if the key is not the id of the box
+          [key]: key !== "id" ? tubesBox[i][key] - 1 : tubesBox[i][key],
         };
-        if (tubes[i][key] === 0) {
-          totalTubesBroken += 1;
-        }
-      } else {tubesBrokenInTheBox += 1}
+        
+        // we ask if the time of life of the tube arrives, if so, we add the count of the total tubes broken
+        if (tubesBox[i][key] === 0) totalTubesBroken += 1;
+        
+        // if the tube doesn't work, we add the count of how many tubes are broken in a single unit 
+      } else tubesBrokenInCurrentBox += 1;
     });
 
-    if (tubesBrokenInTheBox >= 2) {
-      tubes.splice(i, 1);
-      tubes.push(buyNewTubesBox(boxIdCounter));
+    /*
+      we ask if there are two or more tubes (because there is a possibility that two or more tubes has the same useful time 
+      and therefore break in the same time) broken in the current box unit, if so, we change it for another one, and we add 
+      the amount of the box to the total of what the university have spent in the year
+    */
+    if (tubesBrokenInCurrentBox >= 2) {
+      tubesBox.splice(i, 1);
+      tubesBox.push(buyNewTubesBox(boxIdCounter));
       boxIdCounter += 1;
       total += singleTubeInfo.price * 4;
     }
@@ -45,7 +54,7 @@ while(totalTimeInHours > 0) {
 
   totalTimeInHours -= 1;
 }
-console.log(tubes)
+
 console.log(`The total of how much money was spend in tubes is ${total} ${singleTubeInfo.currency}`);
 console.log(`The total of the tubes broken is ${totalTubesBroken} tubes`);
 
--- src/utils.mjs
+++ src/utils.mjs
@@ -7,30 +7,31 @@ export const rand = () => {
     return Math.floor(Math.random() * (maxValue - minValue + 1) + minValue)
 }
 
-export const setupInitialValues = () => {
+export const setupInitialValues = (newBoxId) => {
   let currentTotalPrice = 0;
   let tubesWithLifetime = [];
-  fluorescentTubeUnits.forEach((t, i) => {
-    tubesWithLifetime[i] = {id: t.id}
-    Object.keys(t).forEach((key) => {
-      if(key !== 'id') {
-        tubesWithLifetime[i] = {
-          ...tubesWithLifetime[i],
-          [key]: rand(),
-        };
-        currentTotalPrice += singleTubeInfo.price;
-      }
+    fluorescentTubeUnits.forEach((t, i) => {
+      tubesWithLifetime[i] = {id: t.id}
+      Object.keys(t).forEach((key) => {
+        if(key !== 'id') {
+          tubesWithLifetime[i] = {
+            ...tubesWithLifetime[i],
+            [key]: rand(),
+          };
+          currentTotalPrice += singleTubeInfo.price;
+        }
+      })
     })
-  })
 
-  return {
-    total: currentTotalPrice,
-    tubes: tubesWithLifetime,
-  }
-};
+  return { 
+    total: currentTotalPrice, 
+    tubesBox: tubesWithLifetime 
+  };
+
+}
 
 export const buyNewTubesBox = (id) => {
   let tubes = {id};
   for(let i = 1; i <= tubesPerUnit; i++) { tubes[`tube${i}`] = rand() }
-  return tubes
+  return tubes;
 }
